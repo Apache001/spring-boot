cf10b3f1213e51ddc03d3a3256085ff575ec4b32
王鹏哲 <wangpengzhe@bytedance.com>
2021-03-10 23:41:44 +0800

    mytest branch

---
+++ spring-boot-project/spring_boot_my_test/src/main/java/com/wpz/test/MyApp.java
@@ -0,0 +1,14 @@
+package com.wpz.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author bytedance
+ */
+@SpringBootApplication
+public class MyApp {
+	public static void main(String[] args) {
+		SpringApplication.run(MyApp.class, args);
+	}
+}
